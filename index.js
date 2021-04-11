a6fcf617164a6bc7d096b63b75d2e96809c38e5b
wadad zein <wadad_zein@hotmail.com>
2021-04-11 18:30:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ console.log(writeCards(['Lisa','Kaitlin','Jan'],'surprise'));
 function countDown(number){
   while (number>0, number --);
 }
-console.log(countDown(10));
+console.log(countDown(4));
